053de1abd78462f5d9a53c1dcad11f9d8fee0908
jianhong-git <chenjianhongcn@gmail.com>
2018-06-29 15:06:55 +0800

    Jianhong edited

--- datalistnpy.py
+++ datalistnpy.py
@@ -1,42 +1,32 @@
-#coding=utf-8
+# coding=utf-8
 import nibabel as nib
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-#matplotlib inline
+# matplotlib inline
 
-AD=os.listdir('data/AD/')
-# # #MCI=os.listdir('ADNIprocess/MCI/')
-Normal=os.listdir('data/normal/')
+# AD = os.listdir('data/AD/')
+# MCI = os.listdir('data/MCI/')
+# Normal = os.listdir('data/normal/')
 
-data_AD=[(os.path.join('data/AD/', x), 0) for x in AD]
-# # #data_MCI=[(os.path.join('ADNIprocess/MCI/', x), 2) for x in MCI]
-data_Normal=[(os.path.join('data/normal/', x), 1) for x in Normal]
+# data_AD = [(os.path.join('data/AD/', x), 0) for x in AD]
+# data_MCI = [(os.path.join('data/MCI/', x), 2) for x in MCI]
+# data_Normal = [(os.path.join('data/normal/', x), 1) for x in Normal]
 
-data_list=np.concatenate((data_AD, data_Normal))
-
+# data_list = np.concatenate((data_AD, data_Normal))
+# data_list = np.concatenate((data_list, data_MCI))
 # np.random.shuffle(data_list)
-
-# AD=os.listdir('data/301/eventest/AD/')
-# # # #MCI=os.listdir('ADNIprocess/MCI/')
-# Normal=os.listdir('data/301/eventest/normal/')
-
-# data_AD=[(os.path.join('data/301/eventest/AD/', x), 0) for x in AD]
-# # # #data_MCI=[(os.path.join('ADNIprocess/MCI/', x), 2) for x in MCI]
-# data_Normal=[(os.path.join('data/301/eventest/normal/', x), 1) for x in Normal]
-
-# data_list2=np.concatenate((data_AD, data_Normal))
-
-# np.random.shuffle(data_list2)
-
-# # # np.save('data_list', data_list2)
-# data_list=np.concatenate((data_list, data_list2))
-
-# AD=os.listdir('pred/')
-
-# data=[(os.path.join('pred/', x),0) for x in AD]
-
-#data_list=np.concatenate((data))
+# print(len(data_list))
+# np.save('ADNI_data_list', data_list)
 
 
-np.save('ADNI_data_list', data_list)
+AD = os.listdir('data/301/AD/')
+Normal = os.listdir('data/301/normal/')
+
+data_AD = [(os.path.join('data/301/AD/', x), 0) for x in AD]
+data_Normal = [(os.path.join('data/301/normal/', x), 1) for x in Normal]
+
+data_list = np.concatenate((data_AD, data_Normal))
+np.random.shuffle(data_list)
+print(len(data_list))
+np.save('301_data_list', data_list)
