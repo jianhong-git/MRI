7f7aee9fad7e4f111b5bb8057f804f0ea31ca121
jianhong-git <chenjianhongcn@gmail.com>
2018-08-06 16:03:35 +0800

    Jianhong edited

--- fc.py
+++ fc.py
@@ -135,9 +135,10 @@ np.savetxt('./result/model2/fc2/accuracy.txt',
 #     text_file.write(str(loss_all[:]))
 # with open('./result/model2/accuracy.txt', "w+") as text_file:
 #     text_file.write(str(accuracy_all[:]))
-# saver = tf.train.Saver({'W_fc1': fc['W_fc1'], 'W_fc2': fc['W_fc2'], 'W_fc3': fc['W_fc3'],
-#                         'b_fc1': fc['b_fc1'], 'b_fc2': fc['b_fc2'], 'b_fc3': fc['b_fc3']})
-# save_path = saver.save(sess, "./train/model2/fc/fc.ckpt")
+
+saver = tf.train.Saver({'W_fc1': fc['W_fc1'], 'W_fc2': fc['W_fc2'], 'W_fc3': fc['W_fc3'],
+                        'b_fc1': fc['b_fc1'], 'b_fc2': fc['b_fc2'], 'b_fc3': fc['b_fc3']})
+save_path = saver.save(sess, "./train/model2/fc/fc.ckpt")
 
 time2 = time.time()
 print(time2 - time1)
