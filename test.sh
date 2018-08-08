4955645d2618a97ec89d867aa618c366c65647e0
jianhong-git <chenjianhongcn@gmail.com>
2018-08-08 15:43:01 +0800

    Jianhong edited

--- test.sh
+++ test.sh
@@ -5,5 +5,6 @@
 for batchsize in 8 10 16 32 #{0..10..2}
 do
     echo "$batchsize"  #"Welcome $i times" #$batchsize
-    echo "2**$batchsize"
+    echo ((2**$batchsize))
+    echo $((2**$batchsize))
 done
