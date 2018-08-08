cea2ac8d6c4a426c8c0ba3aaac689fe86cfd2363
jianhong-git <chenjianhongcn@gmail.com>
2018-08-08 15:32:44 +0800

    Jianhong edited

--- test.sh
+++ test.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #!/home/jhchen/anaconda3/bin/python3
 # echo "Bash version ${BASH_VERSION}..."
-# for i in {0..10..2}
-#     do
-#         echo "Welcome $i times"
-#     done
+for i in {0..10..2}
+do
+    echo "Welcome $i times"
+done
 
 
-for power in $(seq 2 6)
-    batchsize=2**$power
-    do
-        echo '$batchsize'
-    done
+for batchsize in 8 10 16 32
+do
+    echo $batchsize
+done
