e466b73cfbf9abc270bc993ee9f64f11696b7712
jianhong-git <chenjianhongcn@gmail.com>
2018-08-08 15:48:33 +0800

    Jianhong edited

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 # echo "Bash version ${BASH_VERSION}..."
 
 for batchsize in 8 10 16 32 #{0..10..2}
-((c=2**$batchsize))
+let 'c=2**$batchsize'
 do
     echo "$batchsize"  #"Welcome $i times" #$batchsize
     echo $c
