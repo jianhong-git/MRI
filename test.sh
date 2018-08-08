3d2e8ed21daf853d13fcca23ba32e7a096cfc208
jianhong-git <chenjianhongcn@gmail.com>
2018-08-08 15:28:10 +0800

    Jianhong edited

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #!/home/jhchen/anaconda3/bin/python3
-echo "Bash version ${BASH_VERSION}..."
-for i in {0..10..2}
-    do
-        echo "Welcome $i times"
-    done
+# echo "Bash version ${BASH_VERSION}..."
+# for i in {0..10..2}
+#     do
+#         echo "Welcome $i times"
+#     done
 
 
 for power in $(seq 2 6)
