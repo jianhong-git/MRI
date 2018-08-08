c3f200788c833034df6f54ebfd7417dca1070ab7
jianhong-git <chenjianhongcn@gmail.com>
2018-08-08 15:03:10 +0800

    ‘upload’

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #!/home/jhchen/anaconda3/bin/python3
 
-for batchsize in {2**'seq 1'}; #'seq 10';{1..10};$(seq 2 6)
+for batchsize in (seq 2); #'seq 10';{1..10};$(seq 2 6)
 do
-    print(batchsize)
+    echo ${batchsize}
 done
