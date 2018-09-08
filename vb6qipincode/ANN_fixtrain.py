b3fc4bc8f901f9579d95fd51f6929ab763c9b593
jianhong-git <chenjianhongcn@gmail.com>
2018-09-08 03:02:30 -0400

    Jianhong edited

--- vb6qipincode/ANN_fixtrain.py
+++ vb6qipincode/ANN_fixtrain.py
@@ -153,8 +153,8 @@ R2_besttrain = 0
 y_best_pred = 0.0
 k = 0
 p = 0.9
-R2 = np.zeros(5 * 10**5)
-Loss = np.zeros(5 * 10**5)
+R2 = np.zeros((5 * 10**4, 1))
+Loss = np.zeros((5 * 10**4, 1))
 for t in range(5 * 10**2):  # 10**5
     # Forward pass: compute predicted y by passing x to the model.
     y_pred = model(Variable(x))
